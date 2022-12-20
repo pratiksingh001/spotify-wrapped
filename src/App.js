87d6c4635c7f9aa71f1f63a7efa1f4627db2651e
Yathaarth-Suri <yathaarth.suri@antino.io>
2022-12-20 11:40:11 +0530

    Installed MUI and logged in into spotify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  // Yathaarth's User ID
+  // const CLIENT_ID = "6d1d7666c23e44dfa4e14c620587fd2d";
+  const REDIRECT_URI = "http://localhost:3000/";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+
+  const handleLogout = () => {
+    setToken("");
+    window.localStorage.removeItem("token", "");
+  };
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+
+      window.localStorage.setItem("token", token);
+      setToken(token);
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      {!token ? (
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+        >
+          Login krlo bhai pehle ek baar
+        </a>
+      ) : (
+        <button onClick={handleLogout}>Logout</button>
+      )}
+    </div>
+  );
 }
 
 export default App;
